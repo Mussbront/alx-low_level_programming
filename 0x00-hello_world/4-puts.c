1b1db9171166cc93980772bdc96f643a918afd9f
Mussbront <vmogendi@gmail.com>
2022-10-14 13:57:38 -0700

    Puts corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
-include < stdio.h >
+#include <stdio.h>
 
+/**
+  * main - Entry point
+  *
+  * Description: prints "Programming is like building a multilingual puzzale"
+  *
+  * Return: always 0 (success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
