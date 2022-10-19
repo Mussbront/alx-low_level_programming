f37c78cf60e6ea86f5d5d38237132d242d39939e
Mussbront <vmogendi@gmail.com>
2022-10-19 08:15:27 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - program that prints _putchar
+  *
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	/**
+	  * _putchar - Prints _putchar
+	  *
+	  *Return: 0 (success)
+	  */
+
+	int _putchar(char c);
+	{
+		write(1, &"_putchar\n", 9);
+		return (0);
+	}
+
+	return (0);
+}
