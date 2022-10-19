780c6f247ad63987fe8750a1164a9be5ba0a82bc
Mussbront <vmogendi@gmail.com>
2022-10-19 09:34:10 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
-  * main - print lowercase alphabet
+  * main - program that prints lowercase alphabet
   *
   * Return: 0 (Success)
   */
 
 int main(void)
 {
-/**
-  * print_alphabet - prints alphabet in lowercase
-  *
-  * Return: 0 (Success)
-  */
+	/**
+	  * print_alphabet - program that prints alphabet in lowercase
+	  *
+	  * Return: 0 (Success)
+	 */
 
 	void print_alphabet(void)
 	{
