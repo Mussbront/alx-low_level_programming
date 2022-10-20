27ec0aff98a2fe287e86848e821a1cda4db35839
Mussbront <vmogendi@gmail.com>
2022-10-19 18:19:55 -0700

    Errors

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 	unsigned long int x = 0, y = 1, s;
 	int i;
 
-	for (i = 1 i <= 98; i++)
+	for (i = 1; i <= 98; i++)
 	{
 		s = x + y;
 
 		if (i != 98)
 		{
-			pritnf("%lu, ", s);
+			printf("%lu, ", s);
 		}
 		else
 		{
