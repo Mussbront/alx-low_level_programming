81cdb2c40bb9467f7cef8387092a4b978d6b7c4c
Mussbront <vmogendi@gmail.com>
2022-10-19 18:14:41 -0700

    still another fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio>
+
+/**
+  * main - prints first 98 fibonacci numbers starting with 1 and 2
+  *
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	unsigned long int x = 0, y = 1, s;
+	int i;
+
+	for (i = 1 i <= 98; i++)
+	{
+		s = x + y;
+
+		if (i != 98)
+		{
+			pritnf("%lu, ", s);
+		}
+		else
+		{
+			printf("%lu\n", s);
+		}
+
+		x = y;
+		y = s;
+	}
+	return (0);
+}
