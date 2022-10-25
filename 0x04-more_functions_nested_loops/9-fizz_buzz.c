30ab9121e40242068e7aefee824a26e6639313a7
Mussbront <vmogendi@gmail.com>
2022-10-25 07:01:04 -0700

    if else

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
-  * main - Print numbers 1-100 repalcing multiples of 3 and 5 with fizz and buzz
+  * main - Print numbers 1-100 repalcing multiples of 3 and 5 with
+  *fizz and buzz
   * Return: Always 0
   */
 
@@ -15,11 +16,10 @@ int main(void)
 		if (n % 5 == 0 && n % 3 == 0)
 			printf("FizzBuzz");
 		else if (n % 5 == 0)
-			if (n == 100)
-				printf("Buzz");
-			else
-				printf("Buzz");
-		else if (n % 3 % == % 0)
+			printf("Buzz");
+		else if (n == 100)
+			printf("Buzz");
+		else if (n % 3 == 0)
 			printf("Fizz");
 		else
 			printf("%d", n);
