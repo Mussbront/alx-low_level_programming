72d8ee8378f16e0a41960c655178a3c26883b775
Mussbront <vmogendi@gmail.com>
2022-10-25 07:26:38 -0700

    space after words and numbers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 			printf("FizzBuzz ");
@@ -23,7 +23,7 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 
