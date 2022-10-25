2468dc5e8cfcb337c8e0bbb4641dfdc37c0bdfec
Mussbront <vmogendi@gmail.com>
2022-10-25 06:39:16 -0700

    if else

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,18 @@
 
 void print_line(int n)
 {
-	int line = 0;
-
-	while (line < n;)
+	int line;
+	
+	if (n <= 0)
 	{
-		_putchar('_');
-		line++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (line =1; line <= n; line++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
