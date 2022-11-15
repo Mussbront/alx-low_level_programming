beeaa611c164b9d798c73810c95701cb8b2ee669
Mussbront <vmogendi@gmail.com>
2022-11-15 10:36:09 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+
+/**
+  * struct dog - structure for dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: name of dog owner
+  */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	
+}
+#endif /* !DOG_H */
