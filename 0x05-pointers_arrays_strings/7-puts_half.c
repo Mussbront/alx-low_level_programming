324be3da8b2215dc915bde6d8c9c3cc6cff23739
Mussbront <vmogendi@gmail.com>
2022-11-16 00:34:04 -0800

    Betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,11 @@ void puts_half(char *str)
 
 	while (str[i++])
 		len++;
-
 	if ((len % 2) == 0)
 		n = len / 2;
 	else
 		n = (len - 1) / 2;
-	
-	for (i = n; i < len; i++)
+	for (n = 0; n < len; n++)
 	{
 		_putchar(str[n]);
 	}
