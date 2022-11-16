62d4523816494b1f2fb5940915dbd3867e33af4c
Mussbront <vmogendi@gmail.com>
2022-11-16 01:28:23 -0800

    for

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,20 +9,14 @@
 
 void print_array(int *a, int n)
 {
-	int i, count;
+	int count;
 
-	i = 0;
-	count = 1;
-	if (n < 0)
-		n = 0;
 	if (n > 0)
-	{
-		while (a[i] != '\0' && count < n)
+		for (count = 0; count < n; count++)
 		{
-			printf("%d, ", a[i]);
-			i++;
-			count++;
+			printf("%d,", a[count]);
+			if (count < n - 1)
+				printf(", ");
 		}
 		printf("%d\n", a[i]);
-	}
 }
