b789489d44785ec013db9f0921a2a9167b0c844e
Mussbront <vmogendi@gmail.com>
2022-11-16 01:38:23 -0800

    ,

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 	if (n > 0)
 	for (count = 0; count < n; count++)
 	{
-		printf("%d,", a[count]);
+		printf("%d", a[count]);
 		if (count < n - 1)
 			printf(", ");
 	}
